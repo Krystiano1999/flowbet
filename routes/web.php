d0a7c4fc4a9cde890bc8a05c60cb370a209aca0f
Krystian Jagiełło <99127311+Krystiano1999@users.noreply.github.com>
2024-11-27 23:12:56 +0100

    Add script js login & register

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('sites.index');
 });
+
+Route::prefix('panel')->group(function () {
+    Route::get('/', function () {
+        return redirect()->route('login.view'); 
+    })->name('panel.home');
+});
+
+Route::prefix('panel')->middleware('guest')->group(function () {
+    Route::view('/register', 'auth.register')->name('register.view');
+    Route::view('/login', 'auth.login')->name('login.view');
+    
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+});
+
+Route::prefix('panel')->middleware('auth')->group(function () {
+
+});
